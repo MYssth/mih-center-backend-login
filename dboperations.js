4c42c1ea67d17f23010bcc8e38a4224823bc4e42
MIH\pongsit <pongsit.po@outlook.com>
2022-12-28 16:53:24 +0700

    update

--- dboperations.js
+++ dboperations.js
@@ -16,13 +16,20 @@ async function login(personnel) {
             console.log("ID found checking password");
             const match = await bcrypt.compare(personnel.personnel_secret, result.recordset[0].personnel_secret);
             if (match) {
-                console.log("Login success prepare jwt for "+personnel.personnel_id);
-                let levelList = await pool.request().input('personnel_id', sql.VarChar, personnel.personnel_id).query("SELECT personnel_level_list.level_id, personnel_levels.mihapp_id FROM personnel_level_list "+
-                "INNER JOIN personnel_levels ON personnel_levels.level_id = personnel_level_list.level_id WHERE personnel_id = @personnel_id");
-                var token = jwt.sign({ "personnel_id": personnel.personnel_id,
-                "personnel_name": result.recordset[0].personnel_firstname+" "+result.recordset[0].personnel_lastname,
-                "level_list": levelList.recordset }, process.env.privateKey, { expiresIn: "4h" });
-                console.log("jwt prepare complete = "+token);
+                if(!result.recordset[0].personnel_isactive){
+                    console.log("user "+personnel.personnel_id+" deactive cannot login");
+                    console.log("====================");
+                    return { "status": "error", "message": "ชื่อผู้ใช้ของคุณอยู่ในสถานะ Deactive กรุณาติดต่อเจ้าหน้าที่เพื่อทำการ Active ชื่อผู้ใช้" };
+                }
+                console.log("Login success prepare jwt for " + personnel.personnel_id);
+                let levelList = await pool.request().input('personnel_id', sql.VarChar, personnel.personnel_id).query("SELECT personnel_level_list.level_id, personnel_level_list.view_id, personnel_levels.mihapp_id FROM personnel_level_list " +
+                    "INNER JOIN personnel_levels ON personnel_levels.level_id = personnel_level_list.level_id WHERE personnel_id = @personnel_id");
+                var token = jwt.sign({
+                    "personnel_id": personnel.personnel_id,
+                    "personnel_name": result.recordset[0].personnel_firstname + " " + result.recordset[0].personnel_lastname,
+                    "level_list": levelList.recordset
+                }, process.env.privateKey, { expiresIn: "4h" });
+                console.log("jwt prepare complete = " + token);
                 console.log("====================");
                 return { "status": "ok", "message": "เข้าสู่ระบบสำเร็จ", token };
             }
@@ -47,7 +54,7 @@ async function login(personnel) {
 async function authen(token) {
 
     try {
-        console.log("authen request from "+token);
+        console.log("authen request from " + token);
         console.log("====================");
         var decoded = jwt.verify(token, process.env.privateKey);
         return { "status": "ok", decoded };
@@ -59,7 +66,42 @@ async function authen(token) {
 
 }
 
+async function verify(personnel_id, personnel_secret) {
+
+    try {
+        console.log("verify request try connect to server");
+        let pool = await sql.connect(config);
+        console.log("connect complete");
+        let result = await pool.request().input('personnel_id', sql.VarChar, personnel_id).query("SELECT personnel_secret FROM personnel WHERE personnel_id = @personnel_id");
+        console.log("verify for id = " + personnel_id);
+        if (result.recordset.length != 0) {
+            const match = await bcrypt.compare(personnel_secret, result.recordset[0].personnel_secret);
+            if (match) {
+                console.log("verify for " + personnel_id + " success");
+                console.log("====================");
+                return { "status": "ok", "message": "ตรวจสอบสำเร็จ" };
+            }
+            else {
+                console.log("verify for " + personnel_id + " fail secret not match");
+                console.log("====================");
+                return { "status": "error", "message": "รหัสผ่านยืนยันไม่ถูกต้อง" };
+            }
+        }
+        else {
+            console.log("data for " + personnel_id + " not found");
+            console.log("====================");
+            return { "status": "error", "message": "ไม่พบชื่อผู้ใช้" };
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return { "status": "error", "message": error.message };
+    }
+
+}
+
 module.exports = {
     login: login,
     authen: authen,
+    verify: verify,
 }
--- server.js
+++ server.js
@@ -4,24 +4,23 @@ var personnel = require('./personnel');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+const { request, response } = require('express');
 var app = express();
 var router = express.Router();
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: '*'
-}));
 app.use('/api', router);
 
 router.use((request, response, next) => {
     //write authen here
 
-    response.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    response.setHeader('Access-Control-Allow-Credentials', true);
-
+    response.setHeader('Access-Control-Allow-Origin', '*')
     console.log('middleware');
     next();
 });
@@ -42,6 +41,17 @@ router.route('/authen').post((request, response) => {
 
     const token = request.headers.authorization.split(' ')[1];
     dboperations.authen(token).then(result => {
+        response.json(result);
+    }).catch(err => {
+        console.error(err);
+        response.sendStatus(500);
+    });
+
+});
+
+router.route('/verify/:id/:secret').get((request, response) => {
+    
+    dboperations.verify(request.params.id, request.params.secret).then(result => {
         response.json(result);
     }).catch(err => {
         console.error(err);
