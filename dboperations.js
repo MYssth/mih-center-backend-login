00c35f448923505af93463636cd80b2ef911c839
pongsit-po <pongsit.po@outlook.com>
2023-08-09 11:39:55 +0700

    update v2.3  - api gateway support

--- dboperations.js
+++ dboperations.js
@@ -1,132 +1,155 @@
-require('dotenv').config();
-var config = require('./dbconfig');
-const sql = require('mssql');
-const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+// require('dotenv').config()
+var config = require("./dbconfig");
+const sql = require("mssql");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 
-async function getSiteSettingData(){
-    
-    console.log("let getSiteSettingData");
-    const controller = new AbortController();
-    const signal = controller.signal;
-    const result = await fetch(`http://${process.env.backendHost}:${process.env.roleCrudPort}/api/getsitesetting`, { signal })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("getSiteSettingData complete");
-            return data;
-        })
-        .catch((error) => {
-            if (error.name === "AbortError") {
-                console.log("cancelled");
-            }
-            else {
-                console.error('Error:', error);
-            }
-        });
-    controller.abort();
-    return result;
-}
+// async function getSiteSettingData(){
+
+//     console.log("let getSiteSettingData");
+//     const controller = new AbortController();
+//     const signal = controller.signal;
+//     const result = await fetch(`http://${process.env.backendHost}:${process.env.roleCrudPort}/api/getsitesetting`, { signal })
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log("getSiteSettingData complete");
+//             return data;
+//         })
+//         .catch((error) => {
+//             if (error.name === "AbortError") {
+//                 console.log("cancelled");
+//             }
+//             else {
+//                 console.error('Error:', error);
+//             }
+//         });
+//     controller.abort();
+//     return result;
+// }
 
 async function login(personnel) {
-
-    try {
-        console.log("login request try to connect server");
-        let pool = await sql.connect(config);
-        console.log("connect complete");
-        let result = await pool.request().input('personnel_id', sql.VarChar, personnel.personnel_id).query("SELECT * FROM personnel WHERE personnel_id = @personnel_id");
-        console.log("Login check id = " + personnel.personnel_id);
-        if (result.recordset.length != 0) {
-            console.log("ID found checking password");
-            const match = await bcrypt.compare(personnel.personnel_secret, result.recordset[0].personnel_secret);
-            if (match) {
-                if(!result.recordset[0].personnel_isactive){
-                    console.log("user "+personnel.personnel_id+" deactive cannot login");
-                    console.log("====================");
-                    return { "status": "error", "message": "ชื่อผู้ใช้ของคุณอยู่ในสถานะ Deactive กรุณาติดต่อเจ้าหน้าที่เพื่อทำการ Active ชื่อผู้ใช้" };
-                }
-                console.log("Login success prepare jwt for " + personnel.personnel_id);
-                let levelList = await pool.request().input('personnel_id', sql.VarChar, personnel.personnel_id).query("SELECT personnel_level_list.level_id, personnel_level_list.view_id, personnel_levels.mihapp_id FROM personnel_level_list " +
-                    "INNER JOIN personnel_levels ON personnel_levels.level_id = personnel_level_list.level_id WHERE personnel_id = @personnel_id");
-                const siteInfo = await getSiteSettingData();
-                var token = jwt.sign({
-                    "personnel_id": personnel.personnel_id,
-                    "personnel_name": result.recordset[0].personnel_firstname + " " + result.recordset[0].personnel_lastname,
-                    "level_list": levelList.recordset,
-                    "site_info": siteInfo 
-                }, process.env.privateKey, { expiresIn: "4h" });
-                console.log("jwt prepare complete = " + token);
-                console.log("====================");
-                return { "status": "ok", "message": "เข้าสู่ระบบสำเร็จ", token };
-            }
-            else {
-                console.log("Wrong password");
-                console.log("====================");
-                return { "status": "error", "message": "รหัสผ่านไม่ถูกต้อง" };
-            }
+  try {
+    console.log("login request try to connect server");
+    console.log(config);
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    let result = await pool
+      .request()
+      .input("personnel_id", sql.VarChar, personnel.personnel_id)
+      .query("SELECT * FROM personnel WHERE personnel_id = @personnel_id");
+    console.log("Login check id = " + personnel.personnel_id);
+    if (result.recordset.length != 0) {
+      console.log("ID found checking password");
+      const match = await bcrypt.compare(
+        personnel.personnel_secret,
+        result.recordset[0].personnel_secret
+      );
+      if (match) {
+        if (!result.recordset[0].personnel_isactive) {
+          console.log(
+            "user " + personnel.personnel_id + " deactive cannot login"
+          );
+          console.log("====================");
+          return {
+            status: "error",
+            message:
+              "ชื่อผู้ใช้ของคุณอยู่ในสถานะ Deactive กรุณาติดต่อเจ้าหน้าที่เพื่อทำการ Active ชื่อผู้ใช้",
+          };
         }
-        else {
-            console.log("User not found");
-            console.log("====================");
-            return { "status": "error", "message": "ไม่พบชื่อผู้ใช้" };
-        }
+        console.log("Login success prepare jwt for " + personnel.personnel_id);
+        let levelList = await pool
+          .request()
+          .input("personnel_id", sql.VarChar, personnel.personnel_id)
+          .query(
+            "SELECT personnel_level_list.level_id, personnel_level_list.view_id, personnel_levels.mihapp_id FROM personnel_level_list " +
+              "INNER JOIN personnel_levels ON personnel_levels.level_id = personnel_level_list.level_id WHERE personnel_id = @personnel_id"
+          );
+        // const siteInfo = await getSiteSettingData();
+        // console.log(siteInfo);
+        var token = jwt.sign(
+          {
+            personnel_id: personnel.personnel_id,
+            personnel_name:
+              result.recordset[0].personnel_firstname +
+              " " +
+              result.recordset[0].personnel_lastname,
+            level_list: levelList.recordset,
+            // "site_info": siteInfo
+          },
+          process.env.privateKey,
+          { expiresIn: "4h", issuer: process.env.iss, algorithm: "HS256" }
+        );
+        console.log("jwt prepare complete = " + token);
+        console.log("====================");
+        return { status: "ok", message: "เข้าสู่ระบบสำเร็จ", token };
+      } else {
+        console.log("Wrong password");
+        console.log("====================");
+        return { status: "error", message: "รหัสผ่านไม่ถูกต้อง" };
+      }
+    } else {
+      console.log("User not found");
+      console.log("====================");
+      return { status: "error", message: "ไม่พบชื่อผู้ใช้" };
     }
-    catch (error) {
-        console.error(error);
-        return { "status": "error", "message": error.message };
-    }
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
 }
 
 async function authen(token) {
-
-    try {
-        console.log("authen request from " + token);
-        console.log("====================");
-        var decoded = jwt.verify(token, process.env.privateKey);
-        return { "status": "ok", decoded };
-    }
-    catch (error) {
-        console.error(error);
-        return { "status": "error", "message": error.message };
-    }
-
+  try {
+    console.log("authen request from " + token);
+    console.log("====================");
+    var decoded = jwt.verify(token, process.env.privateKey);
+    return { status: "ok", decoded };
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
 }
 
 async function verify(personnel_id, personnel_secret) {
-
-    try {
-        console.log("verify request try connect to server");
-        let pool = await sql.connect(config);
-        console.log("connect complete");
-        let result = await pool.request().input('personnel_id', sql.VarChar, personnel_id).query("SELECT personnel_secret FROM personnel WHERE personnel_id = @personnel_id");
-        console.log("verify for id = " + personnel_id);
-        if (result.recordset.length != 0) {
-            const match = await bcrypt.compare(personnel_secret, result.recordset[0].personnel_secret);
-            if (match) {
-                console.log("verify for " + personnel_id + " success");
-                console.log("====================");
-                return { "status": "ok", "message": "ตรวจสอบสำเร็จ" };
-            }
-            else {
-                console.log("verify for " + personnel_id + " fail secret not match");
-                console.log("====================");
-                return { "status": "error", "message": "รหัสผ่านยืนยันไม่ถูกต้อง" };
-            }
-        }
-        else {
-            console.log("data for " + personnel_id + " not found");
-            console.log("====================");
-            return { "status": "error", "message": "ไม่พบชื่อผู้ใช้" };
-        }
+  try {
+    console.log("verify request try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    let result = await pool
+      .request()
+      .input("personnel_id", sql.VarChar, personnel_id)
+      .query(
+        "SELECT personnel_secret FROM personnel WHERE personnel_id = @personnel_id"
+      );
+    console.log("verify for id = " + personnel_id);
+    if (result.recordset.length != 0) {
+      const match = await bcrypt.compare(
+        personnel_secret,
+        result.recordset[0].personnel_secret
+      );
+      if (match) {
+        console.log("verify for " + personnel_id + " success");
+        console.log("====================");
+        return { status: "ok", message: "ตรวจสอบสำเร็จ" };
+      } else {
+        console.log("verify for " + personnel_id + " fail secret not match");
+        console.log("====================");
+        return { status: "error", message: "รหัสผ่านยืนยันไม่ถูกต้อง" };
+      }
+    } else {
+      console.log("data for " + personnel_id + " not found");
+      console.log("====================");
+      return { status: "error", message: "ไม่พบชื่อผู้ใช้" };
     }
-    catch (error) {
-        console.error(error);
-        return { "status": "error", "message": error.message };
-    }
-
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
 }
 
 module.exports = {
-    login: login,
-    authen: authen,
-    verify: verify,
-}
+  login: login,
+  authen: authen,
+  verify: verify,
+};
--- server.js
+++ server.js
@@ -1,65 +1,75 @@
-const dboperations = require('./dboperations');
-var personnel = require('./personnel');
+const dboperations = require("./dboperations");
+var personnel = require("./personnel");
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-const { request, response } = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+const { request, response } = require("express");
 var app = express();
 var router = express.Router();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api', router);
+app.use("/api/auth", router);
 
 router.use((request, response, next) => {
-    //write authen here
+  //write authen here
 
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    console.log('middleware');
-    next();
+  response.setHeader("Access-Control-Allow-Origin", "*");
+//   console.log("middleware");
+  next();
 });
 
-router.route('/login').post((request, response) => {
-
-    let personnel = { ...request.body }
-    dboperations.login(personnel).then(result => {
-        response.json(result);
-    }).catch(err => {
-        console.error(err);
-        response.sendStatus(500);
-    });
-
+router.route("/health").get((request, response) => {
+    // console.log("health check");
+  response.json({ status: 200 });
 });
 
-router.route('/authen').post((request, response) => {
-
-    const token = request.headers.authorization.split(' ')[1];
-    dboperations.authen(token).then(result => {
-        response.json(result);
-    }).catch(err => {
-        console.error(err);
-        response.sendStatus(500);
+router.route("/login").post((request, response) => {
+  let personnel = { ...request.body };
+  dboperations
+    .login(personnel)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
     });
-
 });
 
-router.route('/verify/:id/:secret').get((request, response) => {
-    
-    dboperations.verify(request.params.id, request.params.secret).then(result => {
-        response.json(result);
-    }).catch(err => {
-        console.error(err);
-        response.sendStatus(500);
+router.route("/authen").post((request, response) => {
+  const token = request.headers.authorization.split(" ")[1];
+  dboperations
+    .authen(token)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
     });
+});
 
+router.route("/verify/:id/:secret").get((request, response) => {
+  dboperations
+    .verify(request.params.id, request.params.secret)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
+    });
 });
 
 var port = process.env.PORT;
 app.listen(port);
-console.log('personnel API is running at ' + port);
+console.log("personnel API is running at " + port);
