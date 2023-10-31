7a1a67ab22601a5b6bf143a4791850de5f14bc19
pongsit-po <pongsit.po@outlook.com>
2023-10-31 11:05:20 +0700

    update v2.4a  - minor fix

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const dboperations = require("./dboperations");
-var personnel = require("./personnel");
 
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -8,27 +7,30 @@ const { request, response } = require("express");
 var app = express();
 var router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 app.use("/api/auth", router);
 
 router.use((request, response, next) => {
   //write authen here
 
-  response.setHeader("Access-Control-Allow-Origin", "*");
-//   console.log("middleware");
+  response.setHeader("Access-Control-Allow-Origin", "*"); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  response.setHeader("Access-Control-Allow-Credentials", true);
+
+  // console.log('middleware');
   next();
 });
 
 router.route("/health").get((request, response) => {
-    // console.log("health check");
+  // console.log("health check");
   response.json({ status: 200 });
 });
 
